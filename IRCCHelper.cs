574cd95cabd243b12755af80edd8dacd64adbd64
hokjoung <hokjoung@gmail.com>
2016-07-28 13:34:02 +0900

    remove unnecessary comments and add FieldIndex for startying byte index

--- IRCCHelper.cs
+++ IRCCHelper.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.BitConverter;
 
-
-namespace IRCC
+namespace Ircc
 {
     public struct Header
     {
@@ -14,48 +13,48 @@ namespace IRCC
         public short code;
         public int size;
         public int reserved;
+
+        public Header(short comm, short code, int size, int reserved)
+        {
+            this.comm = comm;
+            this.code = code;
+            this.size = size;
+            this.reserved = reserved;
+        }
     }
     public struct Packet
     {
         public Header header;
-        public byte[] body;
+        public byte[] data;
+
+        public Packet(Header header, byte[] data)
+        {
+            this.header = header;
+            this.data = data;
+        }
     }
-    public static class IRCCHelper
+    public static class IrccHelper
     {
-        public static byte[] packetToByte(Packet p)
+        public class FieldIndex
+        {
+            public const int COMM = 0;
+            public const int CODE = 2;
+            public const int SIZE = 4;
+            public const int RSVD = 8;
+            public const int DATA = 12;
+        }
+
+        public static byte[] packetToBytes(Packet p)
         {
             /*
-            byte[] msg = new byte[size];
-            
-            msg[0-1] = BitConverter.GetBytes(p.header.comm);
-            msg[2-4] = 
+            byte[] msg = new byte[2 + 2 + 4 + 4 + p.data.Length];
+            Array.Copy(GetBytes(p.header.comm), 0, msg, FieldIndex.COMM, 2);
+            Array.Copy(GetBytes(p.header.code), 0, msg, FieldIndex.CODE, 2);
+            Array.Copy(GetBytes(p.header.size), 0, msg, FieldIndex.SIZE, 4);
+            Array.Copy(GetBytes(p.header.reserved), 0, msg, FieldIndex.RSVD, 4);
+            Array.Copy(p.data, 0, msg, FieldIndex.DATA, p.data.size);
 
-            Array.Copy(GetBytes(p.header.comm), 0, msg, 0, 2);
-            Array.Copy(GetBytes(p.header.code), 0, msg, 2, 2);
-            Array.Copy(GetBytes(p.header.size), 0, msg, 4, 4);
-            Array.Copy(GetBytes(p.header.reserved), 0, msg, 8, 4);
-            Array.Copy(p.body, 0, msg, 12, p.body.size);
-           
-            //////////////////// 
-            comm type (2byte)
-            msg[0] = p.header.comm & 11110000b;
-            msg[1] = p.header.comm & 00001111b;
-
-            code (2byte)
-            msg[2] = p.header.code & 11110000b;
-            msg[3] = p.header.code & 00001111b;
-
-            size (4byte)
-            msg[4] = p.header.size
-            msg[5] = p.header.size
-            msg[6] = p.header.size
-            msg[7] = p.header.size
-
-            reserved (4byte)
-            msg[] = p.header.reserved
-            msg[] = p.header.reserved
-            msg[] = p.header.reserved
-            msg[] = p.header.reserved
+            return msg;
             */
 
             byte[] bComm = GetBytes(p.header.comm);
@@ -63,37 +62,20 @@ namespace IRCC
             byte[] bSize = GetBytes(p.header.size);
             byte[] bRsvd = GetBytes(p.header.reserved);
 
-            return bComm.Concat(bCode).Concat(bSize).Concat(bRsvd).Concat(p.body).ToArray();
+            return bComm.Concat(bCode).Concat(bSize).Concat(bRsvd).Concat(p.data).ToArray();
         }
 
-        /*
-        public static HeaderToByte //send this first
-        public static BodyToByte   //process this while sending and send when finished
-        public static ByteToHeader //get this first
-        public static ByteToPacket //get buffer size from header and receive (give header as argument)
-        */
-         
-
-        public static Packet byteToPacket(byte[] b)
+        public static Packet bytesToPacket(byte[] b)
         {
             Packet p = new Packet();
 
-            p.header.comm = ToInt16(b, 0);
-            p.header.code = ToInt16(b, 2);
-            p.header.size = ToInt32(b, 4);
-            p.header.reserved = ToInt32(b, 8);
-            Array.Copy(b, 12, p.body, 0, p.header.size);
+            p.header.comm = ToInt16(b, FieldIndex.COMM);
+            p.header.code = ToInt16(b, FieldIndex.CODE);
+            p.header.size = ToInt32(b, FieldIndex.SIZE);
+            p.header.reserved = ToInt32(b, FieldIndex.RSVD);
+            Array.Copy(b, FieldIndex.DATA, p.data, 0, p.header.size);
             
             return p;
         }
-
-        /*
-        private static byte[] SubArray(this byte[] data, int index, int length)
-        {
-            byte[] result = new byte[length];
-            Array.Copy(data, index, result, 0, length);
-            return result;
-        }
-        */
     }
 }
