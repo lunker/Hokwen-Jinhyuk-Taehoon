c6dd22df88db79f617b016a3eb52d760329981ab
hokjoung <hokjoung@gmail.com>
2016-08-02 15:08:08 +0900

    update IRCCHelper from main project

--- IRCCHelper.cs
+++ IRCCHelper.cs
@@ -14,6 +14,13 @@ namespace Ircc
         public int size;
         public int reserved;
 
+        public Header(short comm, short code, int size)
+        {
+            this.comm = comm;
+            this.code = code;
+            this.size = size;
+            this.reserved = 0;
+        }
         public Header(short comm, short code, int size, int reserved)
         {
             this.comm = comm;
@@ -35,6 +42,7 @@ namespace Ircc
     }
     public static class IrccHelper
     {
+        public const int HEADER_SIZE = 12;
         public class FieldIndex
         {
             public const int COMM = 0;
@@ -44,7 +52,68 @@ namespace Ircc
             public const int DATA = 12;
         }
 
-        public static byte[] packetToBytes(Packet p)
+        public class Comm
+        {
+            public const short CS = 0;
+            public const short SS = 1;
+            public const short DUMMY = 3;
+        }
+
+        public class Code
+        {
+            // * -> placeholder code for future
+            public const short FAIL         = 000;
+            public const short SUCCESS      = 100;
+
+            public const short MSG          = 200; // normal msg
+            public const short MSG_ERR      = 205; // *
+            public const short SMSG         = 250; // server echo
+            public const short SMSG_ERR     = 255; // *
+
+            public const short SIGNUP       = 310; // signup req
+            public const short SIGNUP_RES   = 312; // signup success
+            public const short SIGNUP_ERR   = 315; // signup fail (duplicate name)
+
+            public const short SIGNIN       = 320; // login req
+            public const short SIGNIN_RES   = 322; // login success
+            public const short SIGNIN_ERR   = 325; // no such name; name & pass mismatch
+
+            public const short LIST         = 400; // room list req
+            public const short LIST_RES     = 402; // room list res
+            public const short LIST_ERR     = 405; // *
+            public const short SLIST        = 450; // server list req
+            public const short SLIST_RES    = 452; // server list res
+            public const short SLIST_ERR    = 455; // *
+
+            public const short JOIN         = 500; // join room req
+            public const short JOIN_RES     = 502; // join room success
+            public const short JOIN_FULL_ERR = 505;// target room full
+            public const short JOIN_NULL_ERR = 510;// target room does not exist
+            public const short SJOIN        = 550; // server join room's echo list req
+            public const short SJOIN_RES    = 552; // server join room's echo list success
+            public const short SJOIN_ERR    = 555; // *
+
+            public const short LEAVE        = 600; // leave room req
+            public const short LEAVE_RES    = 602; // leave room success
+            public const short LEAVE_ERR    = 605; // not valid room / not in target room
+
+            public const short CREATE       = 700; // create room req
+            public const short CREATE_RES   = 702; // create room success
+            public const short CREATE_DUPLICATE_ERR = 705; // room already exists
+            public const short CREATE_FULL_ERR = 710; // room is full
+
+            public const short HEARTBEAT    = 800; // heartbeat
+            public const short HEARTBEAT_RES = 802; // heartbeat res
+
+            public const short DESTROY      = 900; // destroy room req
+            public const short DESTROY_RES  = 902; // destroy room success
+            public const short DESTROY_ERR  = 905; // *
+            public const short SDESTROY     = 950; // server destroy room req
+            public const short SDESTROY_RES = 952; // server destroy room success
+            public const short SDESTROY_ERR = 955; // *
+        }
+       
+        public static byte[] PacketToBytes(Packet p)
         {
             /*
             byte[] msg = new byte[2 + 2 + 4 + 4 + p.data.Length];
@@ -62,10 +131,13 @@ namespace Ircc
             byte[] bSize = GetBytes(p.header.size);
             byte[] bRsvd = GetBytes(p.header.reserved);
 
+            if (null == p.data)
+                return bComm.Concat(bCode).Concat(bSize).Concat(bRsvd).ToArray();
+
             return bComm.Concat(bCode).Concat(bSize).Concat(bRsvd).Concat(p.data).ToArray();
         }
 
-        public static Header bytesToHeader(byte[] b)
+        public static Header BytesToHeader(byte[] b)
         {
             Header h = new Header();
 
@@ -76,8 +148,7 @@ namespace Ircc
 
             return h;
         }
-
-        public static Packet bytesToPacket(byte[] b)
+        public static Packet BytesToPacket(byte[] b)
         {
             Packet p = new Packet();
 
@@ -86,8 +157,67 @@ namespace Ircc
             p.header.size = ToInt32(b, FieldIndex.SIZE);
             p.header.reserved = ToInt32(b, FieldIndex.RSVD);
             Array.Copy(b, FieldIndex.DATA, p.data, 0, p.header.size);
-            
+
             return p;
+        }
+
+        public static Packet CreatePacket(short comm, short code, byte[] bMsg)
+        {
+            Header header;
+            header.comm = comm;
+            header.code = code;
+            header.size = bMsg.Length;
+            header.reserved = 0;
+
+            Packet packet;
+            packet.header = header;
+            packet.data = bMsg;
+
+            //you can send this packet bytes
+            //using socket.send(CreateBytesPacket(comm, code, bytesMsg));
+            return packet;
+        }
+
+        public static Packet CreatePacket(short comm, short code, int reserv, byte[] bMsg)
+        {
+            Header header;
+            header.comm = comm;
+            header.code = code;
+            header.size = bMsg.Length;
+            header.reserved = reserv;
+
+            Packet packet;
+            packet.header = header;
+            packet.data = bMsg;
+
+            //you can send this packet bytes
+            //using socket.send(CreateBytesPacket(comm, code, reserved, bytesMsg));
+            return packet;
+        }
+        public static Packet CreatePacket(short comm, short code)
+        {
+            Header header;
+            header.comm = comm;
+            header.code = code;
+            header.size = 0;
+            header.reserved = 0;
+
+            Packet packet;
+            packet.header = header;
+            packet.data = null; //null data. but is it okay?
+
+            //you can send this packet bytes
+            //using socket.send(CreateBytesPacket(comm, code));
+            return packet;
+        }
+
+        public static string PacketDebug(Packet p)
+        {
+            if(null == p.data)
+                return  "COMM: " + p.header.comm + "\nCODE: " + p.header.code +  "\nSIZE: " + p.header.size + "\nRSVD: " + p.header.reserved + "\nDATA: ";
+            else
+                return "COMM: " + p.header.comm + "\nCODE: " + p.header.code + "\nSIZE: " + p.header.size + "\nRSVD: " + p.header.reserved + "\nDATA: " + Encoding.UTF8.GetString(p.data);
         }
     }
 }
+
